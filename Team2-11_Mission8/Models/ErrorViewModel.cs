2c8528fe14181520f8e814dede0994bd23b81e09
Tyler Sheffield <tylersheffield427@gmail.com>
2024-02-26 08:20:16 -0700

    Add project files.

---
+++ Team2-11_Mission8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Team2_11_Mission8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
